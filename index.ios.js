59b5c742b0c10bc700fc937bc45676a1067ca1c7
vicra <vicramirez_10@hotmail.com>
2016-02-27 21:53:41 -0600

    main file

--- index.ios.js
+++ index.ios.js
@@ -3,30 +3,141 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
-import React, {
+var React = require('react-native');
+var {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  ListView,
+  View,
+  TouchableHighlight,
+  TextInput
+} = React;
+
+var List = [{id:1,name:"Pera"},
+            {id:2,name:"Manzanaz"},
+            {id:3,name:"Aguacate"},
+            {id:4,name:"Fresa"},
+            {id:5,name:"Papa"},
+          {id:6,name:"Litos"}]
+
 
 class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+  constructor(props) {
+    super(props)
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {dataSource:  ds.cloneWithRows(List), _currentAction:"list"};
+    this.edit = this.edit.bind(this)
+    this.Save = this.Save.bind(this)
+    this.delete = this.delete.bind(this)
+    this.create = this.create.bind(this)
+    this.cancel = this.cancel.bind(this)
+    this.add = this.add.bind(this)
+  }
+  edit(Data){
+      this.setState({_currentAction:"edit",  rowData: Data});
+  }
+
+  delete(Data){
+    for(var i=0; i < List.length;i++)
+    {
+      if(List[i].id == Data.id)
+      {
+        List.splice(i,1);
+      }
+    }
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.setState({_currentAction:"list", dataSource:  ds.cloneWithRows(List)});
+  }
+
+  add()
+  {
+      List.push({id:List.length+1, name:this.state.text});
+      var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.setState({_currentAction:"list", dataSource:  ds.cloneWithRows(List)});
+  }
+
+  Save(){
+    for(var i=0; i < List.length;i++)
+    {
+      if(List[i].id == this.state.rowData.id)
+      {
+        List[i].name = this.state.text;
+      }
+    }
+
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.setState({_currentAction:"list", dataSource:  ds.cloneWithRows(List)});
+  }
+
+  create(){
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.setState({_currentAction:"create", text:""});
+  }
+
+  cancel(){
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.setState({_currentAction:"list", dataSource:  ds.cloneWithRows(List)});
+  }
+
+render()
+  {
+      if(this.state._currentAction == "list")
+      {
+          return (
+                <View>
+                    <ListView
+                    style={styles.welcome}
+                    dataSource={this.state.dataSource}
+                    renderRow={
+                    (rowData) =>
+                    <View>
+                        <TouchableHighlight onPress={() => this.edit(rowData)}>
+                          <Text style={styles.listItem}>{rowData.name}</Text>
+                        </TouchableHighlight>
+                    </View>
+                              }/>
+                    <TouchableHighlight onPress={() => this.create()}>
+                      <Text style={styles.listItemAdd}>CREATE</Text>
+                    </TouchableHighlight>
+                </View>
+                );
+     }else if(this.state._currentAction == "edit")
+     {
+         return (
+               <View>
+                   <TextInput
+                   style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                   onChangeText={(text) => this.setState({text})}
+                   defaultValue={this.state.rowData.name}
+                   value={this.state.text} />
+                   <TouchableHighlight onPress={() => this.Save()}>
+                         <Text style={styles.listItem}>SAVE CHANGES</Text>
+                   </TouchableHighlight>
+                   <TouchableHighlight onPress={() => this.delete(this.state.rowData)}>
+                        <Text style={styles.listItemDelete}>DELETE</Text>
+                   </TouchableHighlight>
+               </View>
+               );
+    }else if(this.state._currentAction == "create")
+    {
+        return (
+              <View>
+                  <TextInput
+                  style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                  onChangeText={(text) => this.setState({text})}
+                  value={this.state.text} />
+                  <TouchableHighlight onPress={() => this.add()}>
+                        <Text style={styles.listItemAdd}>ADD</Text>
+                  </TouchableHighlight>
+                  <TouchableHighlight onPress={() => this.cancel()}>
+                       <Text style={styles.listItemDelete}>CANCEL</Text>
+                  </TouchableHighlight>
+              </View>
+              );
+   }
+
   }
 }
 
@@ -38,15 +149,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    flex: 2
   },
-  instructions: {
+  listItem: {
     textAlign: 'center',
+    fontSize:20,
     color: '#333333',
+    borderColor: '#AAAAAA',
+    marginTop: 5,
     marginBottom: 5,
+    marginLeft: 20,
+    marginRight:20,
+    backgroundColor: '#AAAAAA',
   },
+  listItemAdd: {
+    textAlign: 'center',
+    fontSize:20,
+    color: '#333333',
+    borderColor: '#AAAAAA',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 20,
+    marginRight:20,
+    backgroundColor: '#AAEEAA',
+  },
+  listItemDelete: {
+    textAlign: 'center',
+    fontSize:20,
+    color: '#333333',
+    borderColor: '#AAAAAA',
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 20,
+    marginRight:20,
+    backgroundColor: '#EEAAAA',
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
