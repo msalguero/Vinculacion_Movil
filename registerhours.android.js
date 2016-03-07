9d13454c8abd660289594d955c69203a0e260272
HappyFabian <sambagong@gmail.com>
2016-03-07 15:54:09 -0600

    Register Advance

--- registerhours.android.js
+++ registerhours.android.js
@@ -10,27 +10,80 @@ import React, {
   ScrollView,
 } from 'react-native';
 
-
 var ApiService = require('./ApiService');
+var Validations = require('./Validations');
 var api = new ApiService();
 var ds = new ListView.DataSource({rowHasChanged:(row1, row2) => row1 !== row2});
+
 
 class RegisterHours extends Component 
 {
     constructor(props){
       super(props);
-
       var self = this;
-      this.state={dataSource:ds.cloneWithRows([])};
+
+      this.state={
+        selectedMajor:"",
+        selectedSections:"",
+        selectedProjects:"",
+        selectedStudents:"",
+        Majors:[],
+        Sections:[],
+        Projects:[],
+        Students:[]
+      }
+      api.
+    };
+
+    listMajors(item){
+      this.state.Majors.map(function(item){return (<Picker.Item key={item.MajorId} label={item.Name} value={item.Id} />)})
+    };
+
+    setMajors(majors){
+      this.setState({Majors: majors});
+    }
+
+    listSections(item)
+    {
+      this.state.Sections.map(function(item){return (<Picker.Item key={item.Code} label={item.Code} value={item.Id} />)})
+    };
+
+    setSections(sections){
+      this.setState({Sections: sections});
+    }
+
+    listProject(item){
+      this.state.Projects.map(function(item){return (<Picker.Item key={item.Id} label={item.Name} value={item.Id} />)})
+    };
+
+    setProjects(projects){
+      this.setState({Projects: projects});
+    }
+
+    listStudents(item){
+      this.state.Students.map(function(item){return(<Picker.Item key={item.AccountId} label={item.Name} value={item.Id}/>)})
+    };
+
+    setStudents(students){
+      this.setState({Students: students});
+    }
+
+
+    renderForm(){
+
+      return(
+        <View>
+
+        </View>
+        );
     };
 
     render() {
-      return (
-        <View style={styles.app}>
-          <Text>{"Hi"}</Text>
+      return (<View>
+        {this.renderForm()}
         </View>
       );
-    }
+    };
 }
 
 const styles = StyleSheet.create({
